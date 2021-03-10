cdaa9f860491fe8e34b4139fcef7573ed5b4e72e
emartos <enric.martos@gmail.com>
2021-03-10 21:22:47 +0100

    Tests más semánticos

--- src/mooc/test/tv/codely/mooc/courses/application/create/CourseCreatorShould.java
+++ src/mooc/test/tv/codely/mooc/courses/application/create/CourseCreatorShould.java
@@ -1,29 +1,34 @@
 package tv.codely.mooc.courses.application.create;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tv.codely.mooc.courses.CoursesModuleUnitTestCase;
 import tv.codely.mooc.courses.domain.Course;
 import tv.codely.mooc.courses.domain.CourseMother;
-import tv.codely.mooc.courses.domain.CourseRepository;
 import tv.codely.mooc.courses.domain.CreateCourseRequestMother;
 
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
+public final class CourseCreatorShould extends CoursesModuleUnitTestCase {
 
-public final class CourseCreatorShould {
+    private CourseCreator creator;
+
+    @BeforeEach
+    protected void beforeEch() {
+        super.setUp();
+
+        creator = new CourseCreator(repository);
+
+    }
 
     @Test
     public void save_a_valid_course() {
-        CourseRepository repository = mock(CourseRepository.class);
-        CourseCreator creator = new CourseCreator(repository);
-
         CreateCourseRequest request = CreateCourseRequestMother.random();
+
         Course course = CourseMother.fromRequest(request);
 
         creator.create(request);
 
-        verify(repository, atLeastOnce()).save(course);
+        shouldHaveSaved(course);
     }
 
 }
--- src/mooc/test/tv/codely/mooc/courses/infrastructure/persistence/InMemoryCourseRepositoryShould.java
+++ src/mooc/test/tv/codely/mooc/courses/infrastructure/persistence/InMemoryCourseRepositoryShould.java
@@ -1,6 +1,7 @@
 package tv.codely.mooc.courses.infrastructure.persistence;
 
 import org.junit.jupiter.api.Test;
+import tv.codely.mooc.courses.CourseModuleInfrastructureTestCase;
 import tv.codely.mooc.courses.domain.Course;
 import tv.codely.mooc.courses.domain.CourseIdMother;
 import tv.codely.mooc.courses.domain.CourseMother;
@@ -11,31 +12,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
-final class InMemoryCourseRepositoryShould {
+final class InMemoryCourseRepositoryShould extends CourseModuleInfrastructureTestCase {
     @Test
     void save_a_course() {
-        InMemoryCourseRepository repository = new InMemoryCourseRepository();
         Course course = CourseMother.random();
 
-
-        repository.save(course);
+        inMemoryCourseRepository.save(course);
     }
 
     @Test
     void return_an_existing_course() {
-        InMemoryCourseRepository repository = new InMemoryCourseRepository();
-
         Course course = CourseMother.random();
 
-        repository.save(course);
+        inMemoryCourseRepository.save(course);
 
-        assertEquals(Optional.of(course), repository.search(course.id()));
+        assertEquals(Optional.of(course), inMemoryCourseRepository.search(course.id()));
     }
 
     @Test
     void not_return_a_non_existing_course() {
-        InMemoryCourseRepository repository = new InMemoryCourseRepository();
-
-        assertFalse(repository.search(CourseIdMother.random()).isPresent());
+        assertFalse(inMemoryCourseRepository.search(CourseIdMother.random()).isPresent());
     }
 }
